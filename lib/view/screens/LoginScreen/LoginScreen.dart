f2404c4d5054499bc7a3931cffa5cadad435a93a
HeshamQa <qaoudqa@gmail.com>
2023-11-26 11:28:33 +0300

    m

--- lib/view/screens/LoginScreen/LoginScreen.dart
+++ lib/view/screens/LoginScreen/LoginScreen.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:scentlaundry/Size_Config.dart';
+
+import 'Components/MyCustomClipper.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
+      body: Stack(
+        children: [
+          ClipPath(
+            clipper: MyCustomClipper(),
+            child: Container(
+              width: getProportionateScreenWidth(375),
+              height: getProportionateScreenHeight(406),
+              color: const Color(0xffBFDBFE),
+            ),
 
+          )
+
+        ],
+      ),
     );
   }
 }
